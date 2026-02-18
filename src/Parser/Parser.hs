1f3048ae2fd2b112b5bc0fab68af78810286a255
Stephen Hunter Barbella <SHBarbella@gmail.com>
2026-02-18 23:08:24 +0100

    Added some more tests

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -3,6 +3,6 @@ module Parser.Parser where
 import Language.C
 import qualified Data.ByteString.Char8 as BS
 
--- Pure: parse C source code from a ByteString
+-- Parse a bytestring into C code
 parseSource :: BS.ByteString -> Either ParseError CTranslUnit
 parseSource src = parseC src (initPos "<unknown>")
--- test/Parser/ParserTests.hs
+++ test/Parser/ParserTests.hs
@@ -4,6 +4,9 @@ import Test.Hspec
 import Test.HUnit
 import qualified Data.ByteString.Char8 as BS
 import Parser.Parser (parseSource)
+import Language.C
+import Data.Generics (everywhere, mkT)
+
 
 assertParses :: String -> String -> Assertion
 assertParses msg src =
@@ -17,8 +20,39 @@ assertFails msg src =
     Left _  -> return ()
     Right _ -> assertFailure $ msg ++ ": expected failure but succeeded"
 
+testHelloAST :: String -> Assertion
+testHelloAST source = 
+  case parseSource (BS.pack source) of
+    Left err -> assertFailure $ show err
+    Right ast -> assertEqual "AST mismatch" (show $ stripPos expectedAST) (show $ stripPos ast)
+  where
+    stripPos = everywhere (mkT $ \_ -> undefNode :: NodeInfo)
+    
+    expectedAST = CTranslUnit 
+      [CFDefExt (CFunDef
+        [CTypeSpec (CIntType undefNode)]
+        (CDeclr (Just (internalIdent "main")) 
+                [CFunDeclr (Right ([], False)) [] undefNode] 
+                Nothing [] undefNode)
+        []
+        (CCompound []
+          [ CBlockStmt (CExpr (Just (CCall 
+              (CVar (internalIdent "printf") undefNode)
+              [CConst (CStrConst (cString "Hello World") undefNode)]
+              undefNode)) undefNode)
+          , CBlockStmt (CReturn (Just (CConst (CIntConst (cInteger 0) undefNode))) undefNode)
+          ]
+          undefNode)
+        undefNode)]
+      undefNode
+
+
 parserSpec :: Spec
-parserSpec =
+parserSpec = do
+  
+  helloc <- runIO $ readFile "test/c_progs/hello.c"
+  invalidc <- runIO $ readFile "test/c_progs/invalid.c"
+  
   describe "Parser" $ do
     it "parses a simple function" $
       assertParses "simple function" "int main() { return 0; }"
@@ -28,3 +62,12 @@ parserSpec =
 
     it "rejects invalid C" $
       assertFails "invalid C" "@@@not valid C@@@"
+
+    it "parses valid file" $ 
+      assertParses "valid file" helloc
+    
+    it "rejects invalid file" $
+      assertFails "invalid file" invalidc
+
+    it "produces proper AST for hello.c" $
+      testHelloAST helloc
